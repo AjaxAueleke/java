48f01726a3c13c907bbfd626a56625c2b51b6a07
ahmed.jamil7410@gmail.com <ahmed.jamil7410@gmail.com>
2021-05-27 14:29:42 +0500

    added method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
-
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
 public class Main {
+
+    static void readfromfile() {
+        try {
+            String data;
+            File newfile = new File("D:\\news.txt");
+            String[] arr = new String[100];
+            Scanner s = new Scanner(newfile);
+            int i = 0;
+            while (s.hasNext()) {
+
+                data = s.next();
+                arr[i++] = data;
+                System.out.println(data);
+
+            }
+            i--;
+
+            for (; i >= 0; i--) {
+                System.out.println(arr[i]);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error occured in reading file");
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
 	System.out.println("Hello World");
+	readfromfile();
     }
+
 }
